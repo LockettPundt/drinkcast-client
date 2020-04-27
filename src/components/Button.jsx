cfdd223a753e55d8260c4e20983822d476e9719d
Lockett Pundt <kalifornium@hotmail.com>
2020-04-27 10:05:36 -0400

    changedd the color and width of the buttons.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -9,19 +9,23 @@ const ButtonStyled = styled.a`
   margin: 1rem auto;
 
   button {
-    background-color: var(--secondary);
-    color: var(--white);
+    font-family: 'uomo regular';
+    background-color: var(--white);
+    color: var(--black);
+    width: 120px;
     transition: 0.2s ease-in;
-    border-radius: 5px;
+    border-radius: 3px;
     border-style: none;
     font-weight: bold;
     padding: 10px;
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.5), 0 6px 20px 0 rgba(0, 0, 0, 0.49);
+    z-index: 5;
     cursor: pointer;
   }
 
   button:hover {
-    background-color: var(--white);
-    color: var(--secondary);
+    background-color: var(--black);
+    color: var(--white);
   }
 `;
 
